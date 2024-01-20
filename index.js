c478d9b5477cf41900d72b2d9db797e0d0b6f4bf
shellys <shllygp17.8@gmail.com>
2024-01-20 19:41:25 -0400

    Adding numbers even and odd

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hola Mundo!')
+for (let num = 1; num <= 20; num++) {
+    if (num % 2 === 0) {
+        console.log(num + ' es Par');
+    } else {
+        console.log(num + ' es Impar');
+    }
+}
