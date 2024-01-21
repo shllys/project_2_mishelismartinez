242dd2b76aac7e9f5051b7a018d45449290004a0
shellys <shllygp17.8@gmail.com>
2024-01-21 15:24:19 -0400

    agregamos el archivo HTML que ejecuta los numeros pares e impares

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Se cambia el console 'Hola Mundo' por un bucle for que recorre de menor a mayor, es decir del 1 al 20.
 Con el condicional if, se ejecutara si el numero es par, si no es asi, impar.
 Y estos se mostraran con console.log dando la respuesta de cada recorrido de los números dados(num).
+
+<img src="./icon/division.png" alt="">
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-for (let num = 1; num <= 20; num++) {
-    if (num % 2 === 0) {
-        console.log(num + ' es Par');
-    } else {
-        console.log(num + ' es Impar');
+function ejecutar() {
+    
+    for (let num = 1; num <= 20; num++) {
+        let lista = document.createElement('li');
+        
+        if (num % 2 === 0) {
+            console.log(num + ' es Par');
+            lista.textContent = `${num} es Par`;
+            lista.classList = 'colorOne';
+        } else {
+            console.log(num + ' es Impar');
+            lista.textContent = `${num} es Impar`;
+            lista.classList = 'colorTwo';
+        }
+        let caja = document.querySelector('.answer');
+        caja.append(lista);
     }
 }
